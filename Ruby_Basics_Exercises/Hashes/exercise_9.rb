6e49230a86c528bd23dfc483e39f76459c7f8559
Kestrel Green <kestrel.m.green@gmail.com>
2023-11-11 23:00:39 +0000

    Completed exercises for Hashes section

---
+++ Ruby_Basics_Exercises/Hashes/exercise_9.rb
@@ -0,0 +1,15 @@
+automobiles = {
+  car: {
+    type:  "sedan",
+    color: "blue",
+    year:  2023
+  },
+  
+  truck: {
+    type:  "pickup",
+    color: "red",
+    year:  1998
+  }
+}
+
+p automobiles
